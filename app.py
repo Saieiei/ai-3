e754ffc245c8d4c119773b08e6c96b488f11eec3
Sairudra More <sairudra60@gmail.com>
2024-04-08 18:12:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,13 @@ def getLLamaresponse(input_text,no_words,blog_style):
     ### LLama 2 model
     llm=CTransformers(model='models\llama-2-7b-chat.ggmlv3.q8_0.bin',
                        model_type='llama',
-                       config={'max_new_tokens':256,
+                       config={'max_new_tokens':512,
                                 'temperature':0.01})
 
     ## Prompt Template
 
     template="""
-        Write a blog for {blog_style} job profile for a topic {input_text}
-        within {no_words} words.
+        Write an blog mentioning IPC sections for {blog_style} job profile for a topic {input_text} within {no_words} words.
             """
 
     prompt=PromptTemplate(input_variables=["blog_style", "input_text", 'no_words'],
@@ -32,14 +31,14 @@ def getLLamaresponse(input_text,no_words,blog_style):
 
 
 
-st.set_page_config(page_title="Generate Blogs",
+st.set_page_config(page_title="LAWGPT",
                     page_icon='🤖',
                     layout='centered',
                     initial_sidebar_state='collapsed')
 
-st.header("Generate Blogs 🤖")
+st.header("LAWGPT Blogs 🤖")
 
-input_text=st.text_input("Enter the Blog Topic")
+input_text=st.text_input("Enter the Law Topic/Question")
 
 ##creating to more columns for additional 2 fields
 
@@ -48,8 +47,8 @@ col1, col2=st.columns([5,5])
 with col1:
     no_words=st.text_input('No of Words')
 with col2:
-    blog_style=st.selectbox('Writing teh blog for',
-                            ('Researchers','Data Scientiets','Common People' ),index=0)
+    blog_style=st.selectbox('Writing the blog for',
+                            ('Lawyers','Data Scientiets','Common People' ),index=0)
 
 submit=st.button("Generate")
 
